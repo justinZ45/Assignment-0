433f68499ddce49f321baaeea2985e20eaae5711
justinZ45 <justinzaluk@gmail.com>
2021-12-27 02:51:05 -0500

    Final commit

--- 00-greetFriend/greetFriend.js
+++ 00-greetFriend/greetFriend.js
@@ -1,6 +1,6 @@
 function greetFriend(name) {
-  let greeting = ("Hello, " + name + "!")
-  return greeting
+  let greeting = ("Hello, " + name + "!");  //creates a variable named greeting and initializes it to a new string
+  return greeting;  //returns greeting
 }
 
 // Do not edit this line;
