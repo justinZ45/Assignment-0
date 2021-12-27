a841144d3c8032ab2f89eecb1cc9a687ba1f99b8
justinZ45 <justinzaluk@gmail.com>
2021-12-27 11:14:07 -0500

    Final commit

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,16 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  let wordFreq ={};  //initialize a new object to hold key pair values
+  for(let i =0; i < word.length; i++)
+  {
+  let letter = word.charAt(i);  //variable letter is equal to a character at the index in the word
+  if(wordFreq[letter]){
+  wordFreq[letter]++;  //if letter exists, increment by one
+  }
+  else{
+  wordFreq[letter] =1;  //if new letter is found, set equal to one
+  }
+}
+return wordFreq; //returns final wordFreq object
 }
 
 // Do not edit this line;
