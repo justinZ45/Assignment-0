860d9d069e84f827ca3ee76907c222414fa0fb7c
justinZ45 <justinzaluk@gmail.com>
2021-12-26 21:33:15 -0500

    Final commit

--- 02-subtractFive/subtractFive.js
+++ 02-subtractFive/subtractFive.js
@@ -1,5 +1,6 @@
 function subtractFive(num) {
-  // Insert code here;
+ let newNum = num - 5;   //sets a new number variable equal to the original number subtracted by five
+ return newNum;   //returns the new number
 }
 
 // Do not edit this line;
