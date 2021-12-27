99a173bdb55a2b22c61b681030b5dd89500b1f8c
justinZ45 <justinzaluk@gmail.com>
2021-12-26 21:42:00 -0500

    Final commit

--- 03-cutInHalfAndFloor/cutInHalfAndFloor.js
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -1,5 +1,6 @@
 function cutInHalfAndFloor(num) {
-  // Insert code here;
+  let newNum = Math.floor(num / 2);  //creates a new variable and sets it equal to the passed number divided by 2, and rounded down if necessary
+  return newNum;   //returns the new number
 }
 
 // Do not edit this line;
