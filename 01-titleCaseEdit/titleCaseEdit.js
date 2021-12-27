c88ada2525c086cae00bd60c5a94041e6b8a45ab
justinZ45 <justinzaluk@gmail.com>
2021-12-26 21:26:17 -0500

    Final commit

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,13 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  const arr = title.split(" ");   //splits title into an array of strings when space is encountered
+
+for (let i = 0; i < arr.length; i ++)
+{
+arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);   //traverses array and capitalizes first letter of each word
 }
 
+const newTitle = arr.join(" ");   //creates a new string with new title, separated by spaces
+return newTitle; //returns the final title
+}
 // Do not edit this line;
 module.exports = titleCaseEdit;
