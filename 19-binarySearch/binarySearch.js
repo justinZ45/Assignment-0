1d9d4e06fe60432fd4cb57a3aac7bd3a7195c14c
justinZ45 <justinzaluk@gmail.com>
2021-12-27 12:56:06 -0500

    Final commit

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -4,8 +4,21 @@ class MySolution {
   }
 
   binarySearch(nums, target) {
-    // Insert code here;
+  let mid = Math.floor(nums.length/2); //set middle value, equal to the length of nums, divided by 2
+  let newArr = nums.slice(0);  //create a copy of nums to use within the code
+  if(newArr[mid] == target){  //if middle value equals target, it has been found
+  return true;  //so, return true
   }
+  if(newArr[mid] < target){  //if middle value less than target, recursively call function and check upper half of array
+  return this.binarySearch(newArr.splice(mid, newArr.length-1), target);  
+  }
+  if(newArr[mid] > target){  //if middle value is larger than target, recursively call function and check lower half of array
+  return this.binarySearch(newArr.splice(0, mid), target); 
+  }
+  else{
+  return false;  //if target not found, return false
+  }
+}
 }
 
 // Do not edit this line;
