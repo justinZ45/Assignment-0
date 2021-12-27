6a04d5863cb19f5d535df7a980739baef597771f
justinZ45 <justinzaluk@gmail.com>
2021-12-27 10:36:48 -0500

    Final commit

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,13 @@
 function isPalindrome(word) {
-  // Insert code here;
+  const strLen = word.length;  //creates a const variable equal to the length of the word
+  for(let i = 0; i < strLen/2; i++){
+  {
+   if(word[i] != word[strLen-1-i])  //traverses the first half of word and checks if first and second string are equal
+   {
+   return false;  //output false (not a palindrome)
+   }  
+}
+return true;  //return true (is a palindrome)
 }
 
 // Do not edit this line;
