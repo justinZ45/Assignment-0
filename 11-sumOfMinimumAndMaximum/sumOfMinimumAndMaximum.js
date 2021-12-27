fb5867436e9a98cb65d01b28a91fd1d25372a1e9
justinZ45 <justinzaluk@gmail.com>
2021-12-27 08:47:33 -0500

    Final commit

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,17 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  let min = nums[0];  //create a variable called min that is initialized to the first array element
+  let max = nums[0];  //create a variavle called max that is initialized to the first array element 
+  let sum = 0;
+  for(let i =0; i < nums.length; i++){
+  if(min > nums[i]){ //traverse the array and check if there are any elements smaller than the max
+  min = nums[i];  //if so, min becomes the element at that index
+  }
+  if(max < nums[i]){  //traverse the nums array and check if there are any values greater than the max
+  max = nums[i]  //if so,m max becomes that ekement.
+  }
+ }
+sum = min + max;  //add the min and max, and set that to the sum variable
+return sum; //return the final sum
 }
 
 // Do not edit this line;
